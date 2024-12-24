7497dd7b1418a9df70d8ec619ab6aee90b4b7a2c
Choi Changgyu <cck981007@gmail.com>
2024-12-24 13:40:16 +0900

    account setting

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -13,7 +13,53 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True, required=True)
 
 
+class AccountDeleteSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+
+class PasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ["password"]
+
+    def save(self, **kwargs):
+        user = self.context.get("user")
+        password = self.validated_data["password"]
+        user.set_password(password)  # 비밀번호 해싱 및 저장
+        user.save()
+        return user
+
+
 class AccountDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = "__all__"
+
+
+class AccountUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = [
+            "email",
+            "name",
+            "nickname",
+            "birth_date",
+            "bio",
+            "gender",
+        ]
+        extra_kwargs = {
+            "email": {"required": True},
+            "name": {"required": True},
+            "nickname": {"required": True},
+            "birth_date": {"required": True},
+            "bio": {"required": False},
+            "gender": {"required": False},
+        }
+
+    def validate_email(self, value):
+        if Account.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "This email is already taken by another user."
+            )
+
+        return value
--- accounts/urls.py
+++ accounts/urls.py
@@ -8,10 +8,9 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("", views.signup, name="account_signup"),
+    path("", views.AccountAPIView.as_view(), name="account_signup"),
     path("login/", views.signin, name="account_signin"),
     path("logout/", views.logout, name="account_logout"),
-    path("<str:username>/", views.profile, name="account_profile"),
-    # path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("password/", views.password_update, name="account_password"),
+    path("<str:username>/", views.UsernameAPIView.as_view(), name="account_profile"),
 ]
